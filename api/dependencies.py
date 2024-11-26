73ee68d99c15036f077e967dba2685cd38f21b3c
Viktors Makreckis <viktors.makreckis@cygnia.net>
2024-11-26 11:18:43 +0000

    Add dependencies.py

---
+++ api/dependencies.py
@@ -0,0 +1,12 @@
+from typing import Annotated
+from dotenv import dotenv_values
+from fastapi import Depends
+from sqlmodel import Session
+
+from api.database import get_session
+
+secrets = dotenv_values('.env.local')
+
+DATABASE_URL = secrets['NEON_DATABASE_URL']
+
+SessionDependency = Annotated[Session, Depends(get_session)]
