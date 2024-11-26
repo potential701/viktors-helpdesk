fbd54d1f5ba521cb2df8b0b0c60d6964f8e8f927
Viktors Makreckis <viktors.makreckis@cygnia.net>
2024-11-26 11:18:52 +0000

    Add models.py

---
+++ api/models.py
@@ -0,0 +1,8 @@
+from sqlmodel import SQLModel, Field
+
+
+class User(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    username: str = Field(index=True)
+    password: str = Field()
+    access_level: str = Field()
