1006f4d7fb71a8a04459e14cffa530eb10b14e74
Viktors Makreckis <viktors.makreckis@cygnia.net>
2024-11-26 11:18:36 +0000

    Add database.py

---
+++ api/database.py
@@ -0,0 +1,17 @@
+from dotenv import dotenv_values
+from sqlmodel import create_engine, SQLModel, Session
+
+secrets = dotenv_values('.env.local')
+
+DATABASE_URL = secrets['NEON_DATABASE_URL']
+
+engine = create_engine(DATABASE_URL)
+
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+
+def get_session():
+    with Session(engine) as session:
+        yield session
