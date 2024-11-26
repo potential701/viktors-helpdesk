350f314eca843ba7351711505ef5ee14375e3304
Viktors Makreckis <viktors.makreckis@cygnia.net>
2024-11-26 15:15:05 +0000

    Add sonner

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type {Metadata} from "next";
 import {Inter} from 'next/font/google'
 import "./globals.css";
+import {Toaster} from "sonner";
 
 const font = Inter({subsets: ['latin']});
 
@@ -20,6 +21,7 @@ export default function RootLayout(
     <body className={`${font.className}`}
     >
     {children}
+    <Toaster />
     </body>
     </html>
   );
