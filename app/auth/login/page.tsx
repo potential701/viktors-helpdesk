4816ce860d86d9156de538ca5673c6a2d1feb033
Viktors Makreckis <viktors.makreckis@cygnia.net>
2024-11-27 15:16:38 +0000

    Add <LoginForm />

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,7 +1,9 @@
+import LoginForm from "@/app/auth/login/login-form";
+
 export default function Page(){
   return (
-    <main>
-      Login
+    <main className='max-w-7xl mx-auto'>
+      <LoginForm />
     </main>
   );
 }
