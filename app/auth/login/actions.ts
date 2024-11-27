6a3ce32a240985b3fb24324d2cd881c76659827d
Viktors Makreckis <viktors.makreckis@cygnia.net>
2024-11-27 15:15:06 +0000

    Set response token as cookie, show toasts

--- app/auth/login/actions.ts
+++ app/auth/login/actions.ts
@@ -2,17 +2,21 @@
 
 import axios from 'axios';
 import {getErrorMessage} from '@/lib/utilities';
+import {cookies} from "next/headers";
 
-export async function loginUser(formData: FormData){
-  try{
+export async function loginUser(formData: FormData): Promise<string> {
+  const cookieStore = await cookies()
+
+  try {
     const response = await axios.post(process.env.BASE_URL + '/api/login', {
       username: formData.get('username'),
       password: formData.get('password')
     })
 
+    cookieStore.set('auth_token', response.data.token, {expires: new Date(response.data.expires_at)})
+
     return response.data.message;
-  }
-  catch (error) {
+  } catch (error) {
     throw new Error(getErrorMessage(error));
   }
 }
