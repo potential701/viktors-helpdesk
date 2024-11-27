8719d8723f9c3adb7360416f3d16ab657561cfc8
Viktors Makreckis <viktors.makreckis@cygnia.net>
2024-11-27 15:15:25 +0000

    Add button variants with class-variance-authority

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -1,6 +1,40 @@
-import {Button as HeadlessButton, ButtonProps} from "@headlessui/react";
+import {Button as HeadlessButton, ButtonProps as HeadlessButtonProps} from "@headlessui/react";
 import {cn} from "@/lib/utilities";
+import {cva, type VariantProps} from 'class-variance-authority'
 
-export default function Button(props: ButtonProps){
-  return <HeadlessButton {...props} className={cn('py-2 px-4 bg-neutral-100 text-neutral-900 rounded-lg border font-medium tracking-tight', props.className)}>{props.children}</HeadlessButton>
+const variants = cva([
+  'py-2 px-4 rounded-lg font-medium tracking-tight transition duration-100 data-[disabled]:cursor-not-allowed'
+], {
+  variants: {
+    variant: {
+      primary: [
+        'bg-teal-400 text-neutral-950 border border-teal-400 data-[hover]:bg-teal-500 data-[hover]:border-teal-500',
+        'data-[active]:scale-95 data-[disabled]:bg-teal-300 data-[disabled]:border-teal-300 data-[disabled]:text-neutral-600'
+      ],
+      secondary: ''
+    },
+    size: {
+      default: '',
+      sm: '',
+      lg: ''
+    }
+  },
+  defaultVariants: {
+    variant: 'primary',
+    size: 'default'
+  }
+})
+
+interface ButtonProps extends HeadlessButtonProps, VariantProps<typeof variants> {
+}
+
+export default function Button({className, variant, size, ...props}: ButtonProps) {
+  return (
+    <HeadlessButton
+      {...props}
+      className={cn(variants({variant, size, className}))}
+    >
+      {props.children}
+    </HeadlessButton>
+  );
 }
