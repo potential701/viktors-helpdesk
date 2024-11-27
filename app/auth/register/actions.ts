d2aeb5f7ae55ab056c564668ac98f7fffdb26f83
Viktors Makreckis <vmakreckis2809@gmail.com>
2024-11-27 10:22:58 +0000

    Change base url to environment variable

--- app/auth/register/actions.ts
+++ app/auth/register/actions.ts
@@ -5,7 +5,7 @@ import {getErrorMessage} from "@/lib/utilities";
 
 export async function registerUser(formData: FormData): Promise<string> {
   try {
-    const response = await axios.post('http://localhost:3000/api/register', {
+    const response = await axios.post(process.env.BASE_URL + '/api/register', {
       username: formData.get('username'),
       password: formData.get('password'),
       access_level: formData.get('access-level')
