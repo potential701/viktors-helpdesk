a87f4bd715e36f63c5602b0268d79940dbfcb827
Viktors Makreckis <vmakreckis2809@gmail.com>
2024-11-27 10:23:23 +0000

    Add login and verify endpoints

--- api/index.py
+++ api/index.py
@@ -1,11 +1,16 @@
+from time import gmtime
+
 import bcrypt
+import jwt
+import datetime
+
 from fastapi import FastAPI, status, HTTPException
 from fastapi.responses import JSONResponse
 from sqlmodel import select
 from scalar_fastapi import get_scalar_api_reference
 
 from api.database import create_db_and_tables
-from api.dependencies import SessionDependency
+from api.dependencies import SessionDependency, JWT_SECRET
 from api.models import User
 
 # Create FastAPI instance
@@ -20,6 +25,7 @@ def on_startup():
 @app.get('/api/hello')
 def hello():
     return JSONResponse(status_code=status.HTTP_200_OK, content={'message': 'Hello, World!'})
+
 
 @app.get('/api/scalar')
 async def scalar_html():
@@ -39,3 +45,29 @@ async def register(user: User, session: SessionDependency):
     session.commit()
 
     return JSONResponse(status_code=status.HTTP_201_CREATED, content={'message': 'User registered successfully.'})
+
+
+@app.post('/api/login')
+async def login(user: User, session: SessionDependency):
+    db_user = session.exec(select(User).where(User.username == user.username)).first()
+    if not db_user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='User not found.')
+
+    is_password_correct = bcrypt.checkpw(user.password.encode('utf-8'), db_user.password.encode('utf-8'))
+    if not is_password_correct:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Username or password is incorrect.')
+
+    token = jwt.encode(payload={'exp':datetime.datetime.now() + datetime.timedelta(minutes=2)}, key=JWT_SECRET, algorithm='HS256')
+
+    return JSONResponse(status_code=status.HTTP_202_ACCEPTED, content={'message': 'Login successful.', 'token': token})
+
+
+@app.post('/api/verify')
+async def verify(token: str):
+    try:
+        jwt.decode(token, key=JWT_SECRET, algorithms=['HS256'])
+        return JSONResponse(status_code=status.HTTP_200_OK, content={'message': 'Token is valid.'})
+    except jwt.ExpiredSignatureError:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Token has expired.')
+    except:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Token is invalid.')
