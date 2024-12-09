c9f02bfa38277b1ca3783f04590ab80e8ef6b5a8
Viktors Makreckis <vmakreckis2809@gmail.com>
2024-12-09 18:52:56 +0000

    Verify authentication in middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,3 +1,35 @@
-export async function middleware(){
+import {NextRequest, NextResponse} from 'next/server';
+import axios from 'axios';
+import {cookies} from 'next/headers';
 
+const unauthenticatedRoutes = ['/auth/login', '/auth/register']
+
+export async function middleware(request: NextRequest) {
+  const cookieStore = await cookies();
+  const auth_token = cookieStore.get('auth_token');
+
+  try {
+    await axios.get(process.env.NEXT_PUBLIC_BASE_URL + '/api/verify?token=' + auth_token?.value)
+
+    if (unauthenticatedRoutes.includes(request.nextUrl.pathname)) {
+      return NextResponse.redirect(new URL('/', request.url))
+    }
+  } catch {
+    if (!unauthenticatedRoutes.includes(request.nextUrl.pathname)) {
+      return NextResponse.redirect(new URL('/auth/login', request.url))
+    }
+  }
+}
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico, sitemap.xml, robots.txt (metadata files)
+     */
+    '/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)',
+  ],
 }
