0fb0a9c0c35e974dfabe7ac79d6891eeabc55350
Viktors Makreckis <viktors.makreckis@cygnia.net>
2024-12-09 13:51:31 +0000

    Change api rewrites host and port

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: process.env.NODE_ENV === 'development' ? 'http://127.0.0.1:8000/api/:path*' : '/api/',
+        destination: process.env.NODE_ENV === 'development' ? 'http://127.0.0.1:8000/api/:path*' : 'http://0.0.0.0:8000/api/',
       }
     ]
   }
