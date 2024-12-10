aa6e98ac3db551d8ea5ae0e0af40435ee7c963f1
Viktors Makreckis <viktors.makreckis@cygnia.net>
2024-12-10 16:39:46 +0000

    Add header and styling

--- app/helpdesk/create/page.tsx
+++ app/helpdesk/create/page.tsx
@@ -1,7 +1,11 @@
-export default function Page(){
+import {getUser} from "@/lib/user";
+
+export default async function Page(){
+  const user = await getUser();
+
   return (
-    <main>
-      Create new ticket
+    <main className='max-w-7xl mx-auto px-8 my-12'>
+      <h1 className='text-4xl font-bold tracking-tighter'>New issue</h1>
     </main>
   );
 }
