db5da34fd0be8de4138385d950df99127c552226
Viktors Makreckis <viktors.makreckis@cygnia.net>
2024-12-17 19:38:14 +0000

    Add helpdesk/all page.tsx

---
+++ app/helpdesk/all/page.tsx
@@ -0,0 +1,22 @@
+import axios from "axios";
+import {Category, Issue, User} from "@/lib/types";
+import IssueTable from "@/app/helpdesk/issue-table";
+
+export const dynamic = 'force-dynamic'
+
+export default async function Page() {
+  const issueResponse = await axios.get(process.env.NEXT_PUBLIC_BASE_URL + '/api/issue/read/all');
+  const userResponse = await axios.get(process.env.NEXT_PUBLIC_BASE_URL + '/api/user/read/all');
+  const categoryResponse = await axios.get(process.env.NEXT_PUBLIC_BASE_URL + '/api/category/read');
+  const issues = issueResponse.data as Issue[];
+  const users = userResponse.data as User[];
+  const categories = categoryResponse.data as Category[];
+
+
+  return (
+    <section className='space-y-4'>
+      <h1 className='text-3xl font-medium tracking-tighter'>All issues</h1>
+      <IssueTable issues={issues} users={users} categories={categories} />
+    </section>
+  );
+}
